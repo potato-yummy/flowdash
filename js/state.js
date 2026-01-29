f1774e57aee5db4602d29c270f2c347beaca5bbd
patato-yummy <ehdwo2024@gmail.com>
2026-01-29 15:04:11 +0900

    오름차순 정렬과 뱃지가 맞지않는 부분 수정

--- js/state.js
+++ js/state.js
@@ -1,10 +1,10 @@
 let todoList = [];
 let editTargetId = null;
-let selectedPriority = '낮음';
+let selectedPriority = "낮음";
 
 const currentFilters = {
-  search: '',
-  term: '전체 기간',
-  priority: '전체 우선순위',
-  sortOrder: 'latest',
+  search: "",
+  term: "전체 기간",
+  priority: "전체 우선순위",
+  sortOrder: "asc",
 };
